4db7ab01aec8e12c4cb1b649f37b944757eba939
pongpoti <pong.poti@gmail.com>
2026-03-11 16:30:43 +0000

    v

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import express from "express"
 const app = express()
 const port = process.env.PORT || 8080
-app.use("/video", express.static("video"))
+app.use("/", express.static("public"))
 app.listen(port, () => { console.log("tots-pelvic-course-e-learning is live") })
